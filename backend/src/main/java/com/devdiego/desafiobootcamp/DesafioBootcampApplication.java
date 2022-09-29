21ea3e3a04b10b032a2b5142f15caa8662dada20
Diego <diegorodalmeida@gmail.com>
2022-09-29 15:19:35 -0300

    Created

---
+++ backend/src/main/java/com/devdiego/desafiobootcamp/DesafioBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.devdiego.desafiobootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBootcampApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devdiego/desafiobootcamp/DesafioBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdiego.desafiobootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
